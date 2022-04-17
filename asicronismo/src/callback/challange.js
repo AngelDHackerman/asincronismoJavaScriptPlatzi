3baf05d93063ecf6f3f9ae5a649472124c0b2f45
angelDariaux <angeldariaux@gmail.com>
2022-04-17 16:22:55 -0600

    terminado de usar las callback hell

--- asicronismo/src/callback/challange.js
+++ asicronismo/src/callback/challange.js
@@ -1,4 +1,5 @@
 let XMLHttpsRequest = require('xmlhttprequest').XMLHttpRequest;
+let API = 'https://rickandmortyapi.com/api/character/';
 
 const fetchData = (url_api, callback) => { 
   let xhttp = new XMLHttpsRequest();
@@ -16,6 +17,19 @@ const fetchData = (url_api, callback) => {
   }
   xhttp.send();
 }
+
+fetchData(API, (error1, data1) => {
+  if (error1) return console.error(error1);
+  fetchData(API + data1.results[0].id, function (error2, data2) {
+    if (error2) return console.error(error2);
+    fetchData(data2.origin.url, (error3, data3) => { 
+      if (error3) return console.error(error3);
+      console.log(data1.info.count);
+      console.log(data2.name);
+      console.log(data3.dimension);
+    })
+  })
+});
 
 
 
